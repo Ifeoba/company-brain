d39d021e79e2b6d36114286745831696c1465c45
Ifeoluwa Boboye <32644848+Ifeoba@users.noreply.github.com>
2026-05-25 09:19:08 +0100

    Add company_brain/runtime/workflow_state.py

---
+++ company_brain/runtime/workflow_state.py
@@ -0,0 +1,9 @@
+class WorkflowState:
+    def __init__(self):
+        self.state = {}
+
+    def set(self, key, value):
+        self.state[key] = value
+
+    def get(self, key):
+        return self.state.get(key)
